a5219908a8e465d9265e62a12e7169112d8aa657
jaydin-blake <jaydinblake@gmail.com>
2023-07-11 10:27:30 -0600

    server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8084 });
 
 wss.on("connection", (ws) => {
   console.log("New Client Connected!");
