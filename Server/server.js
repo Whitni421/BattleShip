dd49834038718dfffd1260d0e4cbce11e9848759
jaydin blake <jaydinblake@gmail.com>
2023-07-28 02:01:25 -0600

    fixed sorry

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 // 1: Require web sockets
-const WebSocket = require("wss");
+const WebSocket = require("ws");
 
 const app = express();
 const port = process.env.PORT || 8080;
