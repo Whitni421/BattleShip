6620ffffd2911e9f4e8c14ce51a61d1e5e4d6291
jaydin-blake <136834994+jaydin-blake@users.noreply.github.com>
2023-07-28 01:55:42 -0600

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 // 1: Require web sockets
-const WebSocket = require("ws");
+const WebSocket = require("wss");
 
 const app = express();
 const port = process.env.PORT || 8080;
